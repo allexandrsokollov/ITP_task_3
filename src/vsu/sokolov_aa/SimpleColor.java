03158385612781dc6c07457ef7b5a92400e725cd
allexandersokollov <allexandersokollov@gmail.com>
2021-09-24 21:40:10 +0300

    nothing

---
+++ src/vsu/sokolov_aa/SimpleColor.java
@@ -0,0 +1,13 @@
+package vsu.sokolov_aa;
+
+public enum SimpleColor {
+        BLACK,
+        WHITE,
+        GRAY,
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE;
+    }
+}
