a876a8c8161d54421d32ee70c4d480682627654c
allexandersokollov <allexandersokollov@gmail.com>
2021-09-30 19:19:05 +0300

    task complete

--- src/vsu/sokolov_aa/TesterForGetColor.java
+++ src/vsu/sokolov_aa/TesterForGetColor.java
@@ -1,8 +1,9 @@
 package vsu.sokolov_aa;
 
+import java.util.ArrayList;
+
 public class TesterForGetColor {
-    private static final int SIZE_OF_POINT_COLORS_ARRAY = 10;
-    private static PointColor[] pointColors = new PointColor[SIZE_OF_POINT_COLORS_ARRAY];
+    private static final ArrayList<PointColor> pointColors = new ArrayList<>(36);
 
     private static class PointColor {
         double x;
@@ -17,19 +18,42 @@ public class TesterForGetColor {
     }
 
     public static boolean testGetColor() {
-        pointColors[0] = new PointColor(4, -1, SimpleColor.BLUE);
-        pointColors[1] = new PointColor(0, 0, SimpleColor.GRAY);
-        pointColors[2] = new PointColor(-3, -4, SimpleColor.ORANGE);
-        pointColors[3] = new PointColor(-1, -4, SimpleColor.WHITE);
-        pointColors[4] = new PointColor(6, -3, SimpleColor.GRAY);
-        pointColors[5] = new PointColor(-1, 2, SimpleColor.GRAY);
-        pointColors[6] = new PointColor(-4, -2, SimpleColor.WHITE);
-        pointColors[7] = new PointColor(-2, -2, SimpleColor.GRAY);
-        pointColors[8] = new PointColor(-2, -4, SimpleColor.WHITE);
-        pointColors[9] = new PointColor(3, 0, SimpleColor.BLUE);
+        pointColors.add(new PointColor(1, -6, SimpleColor.GRAY));
+        pointColors.add(new PointColor(2, -5, SimpleColor.GRAY));
+        pointColors.add(new PointColor(3, -4, SimpleColor.GRAY));
+        pointColors.add(new PointColor(3, -3, SimpleColor.GRAY));
+        pointColors.add(new PointColor(3, -2, SimpleColor.GRAY));
+        pointColors.add(new PointColor(2, -1, SimpleColor.GRAY));
+        pointColors.add(new PointColor(-2, -8, SimpleColor.GRAY));
+        pointColors.add(new PointColor(-1, -3, SimpleColor.GRAY));
+        pointColors.add(new PointColor(1, -0, SimpleColor.GRAY));
+        pointColors.add(new PointColor(0, -3, SimpleColor.GRAY));
+        pointColors.add(new PointColor(0, -6, SimpleColor.GRAY));
+        pointColors.add(new PointColor(0, -8, SimpleColor.GRAY));
+        pointColors.add(new PointColor(0, 2, SimpleColor.GRAY));
+        pointColors.add(new PointColor(3, 0, SimpleColor.BLUE));
+        pointColors.add(new PointColor(4, -1, SimpleColor.BLUE));
+        pointColors.add(new PointColor(4, -2, SimpleColor.BLUE));
+        pointColors.add(new PointColor(4, -4, SimpleColor.BLUE));
+        pointColors.add(new PointColor(4, -5, SimpleColor.BLUE));
+        pointColors.add(new PointColor(3, -6, SimpleColor.BLUE));
+        pointColors.add(new PointColor(2, -6, SimpleColor.BLUE));
+        pointColors.add(new PointColor(-3, -5, SimpleColor.ORANGE));
+        pointColors.add(new PointColor(-3, -4, SimpleColor.ORANGE));
+        pointColors.add(new PointColor(-3, -5, SimpleColor.ORANGE));
+        pointColors.add(new PointColor(-3, -6, SimpleColor.ORANGE));
+        pointColors.add(new PointColor(-2, -6, SimpleColor.ORANGE));
+        pointColors.add(new PointColor(-3, -0, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-3, -1, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-2, -5, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-1, -5, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-1, -6, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-1, -7, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-3, -7, SimpleColor.WHITE));
+        pointColors.add(new PointColor(-4, -6, SimpleColor.WHITE));
 
-        for(int i = 0; i <SIZE_OF_POINT_COLORS_ARRAY; i++) {
-            if (Main.getColor(pointColors[i].x, pointColors[i].y) != pointColors[i].color) {
+        for (int i = 0; i < pointColors.size(); i++) {
+            if (Main.getColor(pointColors.get(i).x, pointColors.get(i).y) != pointColors.get(i).color) {
                 return false;
             }
         }
