8e01d69a6760eaaaa456405b2dea90f994b9ac8c
allexandersokollov <allexandersokollov@gmail.com>
2021-09-30 18:50:48 +0300

    parabola fixed should test

--- src/vsu/sokolov_aa/HorizontalParabola.java
+++ src/vsu/sokolov_aa/HorizontalParabola.java
@@ -1,17 +1,17 @@
 package vsu.sokolov_aa;
 
 public class HorizontalParabola {
-    public double x0;
+    public double c;
     public double y0;
     public double a;
 
     public HorizontalParabola(double x0, double y0, double a) {
-        this.x0 = x0;
+        this.c = x0;
         this.y0 = y0;
         this.a = a;
     }
 
     public boolean isPointRightOfParabola(double x, double y) {
-        return x > a * Math.pow(y - y0, 2) + x0;
+        return x > a * Math.pow(y, 2) + c + y0*y;
     }
 }
